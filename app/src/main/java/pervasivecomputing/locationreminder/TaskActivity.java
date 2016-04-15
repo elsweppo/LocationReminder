9d8797b0a58dbea44c7f2c9859c351f306de1bcc
elsweppo <fuller91>
2016-04-15 16:21:42 +0200

    Initial commit

---
+++ app/src/main/java/pervasivecomputing/locationreminder/TaskActivity.java
@@ -0,0 +1,18 @@
+package pervasivecomputing.locationreminder;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by julianschweppe on 15.04.16.
+ */
+public class TaskActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_task);
+
+    }
+
+}
